31e95457b705060e256423b380c4e5cb525831d9
Harsh Sikka <harshsikka123@gmail.com>
2017-10-02 05:53:05 -0400

    Add files via upload

--- defeat_learners/gen_data.py
+++ defeat_learners/gen_data.py
@@ -1,53 +1,24 @@
 """
 template for generating data to fool learners (c) 2016 Tucker Balch
 """
-from random import randint
 import numpy as np
 import math
 
-# this function should return a dataset (X and Y) that will work
-# better for linear regression than decision trees
 def best4LinReg(seed=1489683273):
     np.random.seed(seed)
-    X = np.random.standard_normal(size=(100, 4))
-    Y = np.random.standard_normal(size=(100))
+    X = np.random.standard_normal(size=(100, 16))
+    #map onto an equation so LinRegLearner will have an easier time
+    Y = X[:,0] + 4*X[:,1] + 3*X[:,2] + 2*X[:,3] + X[:,4] + 4*X[:,5] + 3*X[:,6] + 2*X[:,7] + X[:,8] + 4*X[:,9] + 3*X[:,10] + 2*X[:,11] + X[:,12] + 4*X[:,13] + 3*X[:,14] + 2*X[:,15]
     return X, Y
-
-
-# def best4DT(seed=1489683273):
-#     np.random.seed(seed)
-#     X = np.random.random(size=(100,4))
-#     arr = []
-#     # for row in X:
-#     #     index = np.random.randint(0,4)
-#     #     print row[index]
-#     #     arr.append(row[index] * 2)
-#     # Y = np.array(arr)
-
-#     Y = X[:,2] * np.random.random() + X[:,1] * np.random.random()
-#     correlation = np.corrcoef(X[:, 2], Y)
-#     # print correlation
-#     # print X
-#     # print Y
-   
-#     return X, Y
-#     return X, Y
 
 def best4DT(seed=1489683273):
     np.random.seed(seed)
-    X = np.random.normal(size=(100, 50))
-    Y = np.random.standard_normal(size=(100))
-    #Y = np.array([4,4,4,4,4,4,4,4,4,4])
+    # X = np.random.standard_normal(size=(100, 100))
+    # Y = np.random.random(size=(100))
+    X = np.random.random(size = (30,80))
+    np.random.seed(seed)
+    Y = np.random.random(size = (30,80))[:,63]
     return X, Y
-
-
-#best one so far
-
-# def best4DT(seed=1489683273):
-#     np.random.seed(seed)
-#     X = np.random.random(size=(100, 50))
-#     Y = np.random.standard_normal(size=(100))
-#     return X, Y
 
 def author():
     return 'hsikka3' #Change this to your user ID
--- defeat_learners/testbest4.py
+++ defeat_learners/testbest4.py
@@ -3,6 +3,7 @@ Test best4 data generator.  (c) 2016 Tucker Balch
 """
 
 import numpy as np
+import pandas as pd
 import math
 import LinRegLearner as lrl
 import DTLearner as dt
@@ -12,77 +13,111 @@ import time
 # compare two learners' rmse out of sample
 def compare_os_rmse(learner1, learner2, X, Y):
 
-    # compute how much of the data is training and testing
-    train_rows = int(math.floor(0.6* X.shape[0]))
-    test_rows = X.shape[0] - train_rows
+  # compute how much of the data is training and testing
+  train_rows = int(math.floor(0.6* X.shape[0]))
+  test_rows = X.shape[0] - train_rows
 
-    # separate out training and testing data
-    train = np.random.choice(X.shape[0], size=train_rows, replace=False)
-    test = np.setdiff1d(np.array(range(X.shape[0])), train)
-    trainX = X[train, :] 
-    trainY = Y[train]
-    testX = X[test, :]
-    testY = Y[test]
+  # separate out training and testing data
+  train = np.random.choice(X.shape[0], size=train_rows, replace=False)
+  test = np.setdiff1d(np.array(range(X.shape[0])), train)
+  trainX = X[train, :]
+  trainY = Y[train]
+  testX = X[test, :]
+  testY = Y[test]
 
-    # train the learners
-    learner1.addEvidence(trainX, trainY) # train it
-    learner2.addEvidence(trainX, trainY) # train it
+  # train the learners
+  learner1.addEvidence(trainX, trainY) # train it
+  learner2.addEvidence(trainX, trainY) # train it
 
-    # evaluate learner1 out of sample
-    predY = learner1.query(testX) # get the predictions
-    rmse1 = math.sqrt(((testY - predY) ** 2).sum()/testY.shape[0])
+  # evaluate learner1 out of sample
+  predY = learner1.query(testX) # get the predictions
+  rmse1 = math.sqrt(((testY - predY) ** 2).sum()/testY.shape[0])
 
-    # evaluate learner2 out of sample
-    predY = learner2.query(testX) # get the predictions
-    rmse2 = math.sqrt(((testY - predY) ** 2).sum()/testY.shape[0])
+  # evaluate learner2 out of sample
+  predY = learner2.query(testX) # get the predictions
+  rmse2 = math.sqrt(((testY - predY) ** 2).sum()/testY.shape[0])
 
-    return rmse1, rmse2
+  return rmse1, rmse2
+
+
+def ratio(numerator, denominator):
+  return 1.0 if denominator == 0 else numerator / denominator
+
 
 def test_code():
 
-    # create two learners and get data
-    lrlearner = lrl.LinRegLearner(verbose = False)
-    dtlearner = dt.DTLearner(verbose = False, leaf_size = 1)
-    X, Y = best4LinReg(int(np.mod(time.time()*10000, 10000)))
+  # create two learners and get data
+  lrlearner = lrl.LinRegLearner(verbose = False)
+  dtlearner = dt.DTLearner(verbose = False, leaf_size = 1)
+  X, Y = best4LinReg(int(np.mod(time.time()*10000, 10000)))
 
-    # compare the two learners
-    rmseLR, rmseDT = compare_os_rmse(lrlearner, dtlearner, X, Y)
+  # compare the two learners
+  rmseLR, rmseDT = compare_os_rmse(lrlearner, dtlearner, X, Y)
 
-    # share results
-#    print
-#    print "best4LinReg() results"
-#    print "RMSE LR    : ", rmseLR
-#    print "RMSE DT    : ", rmseDT
-    if rmseLR < 0.9 * rmseDT:
-        print '.',
-    else:
-        print "LR >= 0.9 DT:  fail", 'LR RMSE=', rmseLR, 'DT RMSE=', rmseDT
-#    print
+  # share results
+  #    print
+  #    print "best4LinReg() results"
+  #    print "RMSE LR    : ", rmseLR
+  #    print "RMSE DT    : ", rmseDT
+  if rmseLR < 0.9 * rmseDT:
+    print '.',
+  else:
+    print "LR >= 0.9 DT:  fail", 'LR RMSE=', rmseLR, 'DT RMSE=', rmseDT
+  linear_ratio = ratio(rmseLR, rmseDT)
 
-    # get data that is best for a random tree
-    lrlearner = lrl.LinRegLearner(verbose = False)
-    dtlearner = dt.DTLearner(verbose = False, leaf_size = 1)
-    X, Y = best4DT(int(np.mod(time.time()*10000, 10000)))
+  # get data that is best for a random tree
+  lrlearner = lrl.LinRegLearner(verbose = False)
+  dtlearner = dt.DTLearner(verbose = False, leaf_size = 1)
+  X, Y = best4DT(int(np.mod(time.time()*10000, 10000)))
 
-    # compare the two learners
-    rmseLR, rmseDT = compare_os_rmse(lrlearner, dtlearner, X, Y)
-    failures = 0
+  # compare the two learners
+  rmseLR, rmseDT = compare_os_rmse(lrlearner, dtlearner, X, Y)
 
-    # share results
-#    print
-#    print "best4DT() results"
-#    print "RMSE LR    : ", rmseLR
-#    print "RMSE DT    : ", rmseDT
-    if rmseDT < 0.9 * rmseLR:
-        print 'Passed '
-    else:
-        
-        print "DT >= 0.9 LR:  fail", 'DT RMSE=', rmseDT, 'LR RMSE=', rmseLR
-#    print
-    
+  # share results
+  #    print
+  #    print "best4DT() results"
+  #    print "RMSE LR    : ", rmseLR
+  #    print "RMSE DT    : ", rmseDT
+  if rmseDT < 0.9 * rmseLR:
+    print '.',
+  else:
+    print "DT >= 0.9 LR:  fail", 'DT RMSE=', rmseDT, 'LR RMSE=', rmseLR
+  dt_ratio = ratio(rmseDT, rmseLR)
+  return linear_ratio, dt_ratio
 
 start = time.time()
+dt_ratios = []
+linear_ratios = []
 for i in range(100,0,-1) :
-    test_code()
-    if (i%10) == 1 : print i-1
+  lin_r, dt_r = test_code()
+  linear_ratios.append(lin_r)
+  dt_ratios.append(dt_r)
+  if (i%10) == 1 : print i-1
 print 'elapsed time ', time.time()-start, 'secs'
+print
+print 'best4LinReg() ratios summary'
+print pd.Series(linear_ratios).describe()
+print
+print 'best4DT() ratios summary'
+print pd.Series(dt_ratios).describe()
+
+
+# def test_seeds():
+#     total = 1000
+#     success_count = 0
+#     for seed in xrange(total):
+#         lrlearner = lrl.LinRegLearner(verbose=False)
+#         dtlearner = dt.DTLearner(verbose=False, leaf_size=1)
+#         X, Y = best4DT(seed)
+#         rmseLR, rmseDT = compare_os_rmse(lrlearner, dtlearner, X, Y)
+#         if rmseDT < 0.9 * rmseLR:
+#             success_count += 1
+#             if success_count % 100 == 0:
+#                 print("Successful: {}".format(success_count))
+#         else:
+#             print("Failed at seed {}".format(seed))
+#     print("Successful iterations: {} out of {}".format(success_count, total))
+
+# if __name__=="__main__":
+#     # test_code(446)
+#     test_seeds()
